9a826dbdbf0d891b6bfce600aebdab2463a69cdc
lastdont20 <lastdont20@gmail.com>
2026-03-17 20:33:54 +0100

    update

---
+++ lession10.py
@@ -0,0 +1,9 @@
+laptop_details = 'hp laptop 2000 siler'
+product = laptop_details.split(' ')
+print(product)
+laptop1,laptop2,laptop3,laptop4=product
+print('laptop1:', laptop1)
+print('laptop2:', laptop2)
+print('laptop3:', laptop3)
+print('laptop4:', laptop4)
+
